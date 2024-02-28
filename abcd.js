71922fe11fb78b4ecba9cb0962f9b77a3aad0959
AsifIqbal024 <aasifiqbal.me@gmail.com>
2024-02-28 09:30:57 +0600

    Javascript

---
+++ abcd.js
@@ -0,0 +1,165 @@
+// let Koushik = 50 , Asif = 69 , Monowar = 33 , Tanim = 12 ;
+// if(typeof Koushik=='number'&& Koushik>=33) {
+//     console.table('Congratulation Mr.Koushik! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Koushik! You have Failed.')
+// }
+// if(typeof Asif=='number'&& Asif>=33) {
+//     console.table('Congratulation Mr.Asif! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Asif! You have Failed.')
+// }
+// if(typeof Monowar=='number'&& Monowar>=33) {
+//     console.table('Congratulation Mr.Monowar! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Monowar! You have Failed.')
+// }
+// if(typeof Tanim=='number'&& Tanim>=33) {
+//     console.table('Congratulation Mr.Tanim! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Tanim! You have Failed.')
+// }
+
+
+
+
+
+
+
+
+// if(typeof Koushik=='number'&& Koushik>=33) {
+//     console.table('Congratulation Mr.Koushik! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Koushik! You have Failed.')
+// }
+// if(typeof Asif=='number'&& Asif>=33) {
+//     console.table('Congratulation Mr.Asif! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Asif! You have Failed.')
+// }
+// if(typeof Monowar=='number'&& Monowar>=33) {
+//     console.table('Congratulation Mr.Monowar! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Monowar! You have Failed.')
+// }
+// if(typeof Tanim=='number'&& Tanim>=33) {
+//     console.table('Congratulation Mr.Tanim! You have Passed.')
+// }
+// else{
+//     console.table('Sorry Mr.Tanim! You have Failed.')
+// }
+
+
+// let a=16 , b=6 ;
+// switch(true) {
+//     case (b>0 && b<10) :
+//     console.log('b is more than 10');
+//     break;
+//     case(b>10 && b<20) :
+//     console.log('b is 16')
+//     break;
+//     default: console.log ('b is greater than 20')
+
+// }
+
+
+// let a = '20' , b=30 ;
+// if(typeof b=='number'&& b>=30) {
+//     console.log('Pass')
+// }
+// else {
+//     console.log('Fail')
+// }
+// if (typeof a=='string' || a>10) {
+//     console.log('or')
+// }
+// else {
+//     console.log('er')
+// }
+
+
+
+// if(!(a===b)){
+//     console.table('a is number');
+// }
+//     else{
+//         console.table('a is string');
+//     }
+// if(a<16) {
+//     console.log('a is less than 16') ;
+// }
+// else if(a>16){console.log('a is greater than 16')}
+// else{console.log('a is equal to 16')};
+// if(typeof a=='string') {
+//     console.log(typeof a)
+// }
+
+
+
+
+
+// let Kazi = 50 , Asif = 69 , Iqbal = 33 , Monowar = 12 ;
+// switch(true) {
+//     case (Kazi>=0 && Kazi<=32) :
+//         console.log ('Your grade is F');
+//         break;
+//     case (Kazi>=33 && Kazi<=39):
+//         console.log ('Mr. Kazi , Your grade is D') ;
+//         break;
+//     case (Kazi>=40 && Kazi<=49):
+//         console.log ('Mr. Kazi , Your grade is C') ;
+//         break;
+//     case (Kazi>=50 && Kazi<=59):
+//         console.log ('Mr. Kazi , Your grade is B') ;
+//         break;
+//     case (Kazi>=60 && Kazi<=69):
+//         console.log ('Mr. Kazi , Your grade is A-') ;
+//         break;
+//     case (Kazi>=70 && Kazi<=79):
+//         console.log ('Mr. Kazi , Your grade is A') ;
+//         break;
+//     case (Kazi>=80 && Kazi<=100):
+//         console.log ('Mr. Kazi , Your grade is A+') ;
+//         break; 
+//     default: console.log ('Your Result Not Found');
+// }
+// switch(true) {
+//     case (Asif>=0 && Asif<=32) :
+//         console.log ('Mr. Asif , Your grade is F') ;
+//         break;
+//     case (Asif>=33 && Asif<=39):
+//         console.log ('Mr. Asif , Your grade is D') ;
+//         break;
+//     case (Asif>=40 && Asif<=49):
+//         console.log ('Mr. Asif , Your grade is C') ;
+//         break;
+//     case (Asif>=50 && Asif<=59):
+//         console.log ('Mr. Asif , Your grade is B') ;
+//         break;
+//     case (Asif>=60 && Asif<=69):
+//         console.log ('Mr. Asif , Your grade is A-') ;
+//         break;
+//     case (Asif>=70 && Asif<=79):
+//         console.log ('Mr. Asif , Your grade is A') ;
+//         break;
+//     case (Asif>=80 && Asif<=100):
+//         console.log ('Mr. Asif , Your grade is A+') ;
+//         break; 
+//     default: console.log ('Your Result Not Found');
+// }
+
+
+for (let i=0 ; i<=20 ; i++) {
+    console.log (i)
+    if(i='odd numbers') {
+        continue
+    }
+    else console.log (mango)
+}
