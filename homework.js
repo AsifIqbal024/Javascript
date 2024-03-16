c2e942315be6e078f87e7070a894737212245d08
Kazi Asif Iqbal <157669092+AsifIqbal024@users.noreply.github.com>
2024-03-17 00:21:25 +0600

    Add files via upload

--- homework.js
+++ homework.js
@@ -1,5 +1,4 @@
-let A = [50,107, 'jack' , 83 , 'killer' , 'son' , true];
-
+let A = [50,107, 'jack' , 83 , 'killer' , 'son' , 'true'];
 
 // 1. Just show the strings in a new array .
 const B = A.filter(strings=>typeof strings ==='string')
@@ -11,23 +10,29 @@ let C = A.filter(numbers=>typeof numbers ==='number')
 let sortedNumber = C.sort((a,b) => a - b) ;
 console.log (sortedNumber)
 
+
 // 3. Make the string array reverse .
 const reversedArray = B.reverse () ;
 console.log (reversedArray)
+
 
 // 4. Combine both array of numbers and strings .
 const combinedArray = sortedNumber.concat(reversedArray)
 console.log(combinedArray)
 
+
 // 5. Delete 3 items from the 6th item from the combines array .
 
 
 
-// 6. Show elements from A whch are less than 100 .
-
+// 6. Show elements from A which are less than 100 .
+function lessThatHundres (value) {
+    return value < 100 ;}
+const result = A.filter(lessThatHundres)
+console.log (result)
 
 
 // 7. From string array remove the last letter and add word 'str' before each of them .
-
-
-
+const removedLastLetter = B.map(String=>String.slice(0,-1));
+const addedLetters = removedLastLetter.map(i=>'str'+i);
+console.log(addedLetters)
